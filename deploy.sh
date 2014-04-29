f9d532cdf472c693952b7ecd7016cba7612e9dee
Jonathan Levine <tlozoot@gmail.com>
2014-04-28 23:40:11 -0700

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,19 @@ cp -r ./_site/* $tmpdir
 
 git checkout master
 
-echo "Deleting files in master branch"
-for file in *
-do
-    if  [ $file != .git -a $file != .gitignore -a $file != .nojekyll ]; then
-        rm -rf $file
-    fi
-done
+# echo "Deleting files in master branch"
+# for file in *
+# do
+#     if  [ $file != .git -a $file != .gitignore -a $file != .nojekyll ]; then
+#         rm -rf $file
+#     fi
+# done
 
-echo "Copying files from $tmpdir"
-cp -r $tmpdir/* .
+# echo "Copying files from $tmpdir"
+# cp -r $tmpdir/* .
+
+COMMIT_MSG="Last `tail -n 1 ~/kvanderwater.github.io/.git/logs/refs/heads/source | grep "commit: .*" -o`"
+echo $COMMIT_MSG
 
 echo "Removing $tmpdir"
 rm -rf $tmpdir
